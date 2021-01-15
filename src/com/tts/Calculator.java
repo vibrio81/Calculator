3208edfa1808eff1fead5f5102102e9d132fa52c
Lila Stone-Porter <74431023+vibrio81@users.noreply.github.com>
2021-01-14 19:29:18 -0600

    First commit
    its not too shabby

---
+++ src/com/tts/Calculator.java
@@ -0,0 +1,71 @@
+package com.tts;
+
+
+
+    public class Calculator {
+
+        //add method
+        public int add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        //subtract method
+        public int subtract (int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        //multiply method
+        public int multiply (int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        //divide method
+        public int divide (int num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                System.out.println ("num2 cannot be zero");
+                return 0;
+            }
+            else
+                return num1 / num2;
+        }
+
+        //square method
+        public int square (int num1)
+        {
+            return num1 * num1;
+        }}
+
+
+    class MagicCalculator extends Calculator{
+
+
+
+        /*public double squareRoot(int num) {
+            return Math.sqrt(num);
+        }
+
+        public double sin(int num) {
+            return Math.sin(Math.toRadians(num));
+        }
+
+        public double cosine(int num) {
+            return Math.cos(Math.toRadians(num));
+        }
+
+        public double tangent(int num) {
+            return Math.tan(Math.toRadians(num));
+        }
+
+*/
+        public int factorial(int n){
+
+            return (n == 1 || n == 0) ? 1 : n * factorial (n - 1);
+        }
+
+
+
+    }
+
+
---
+++ src/com/tts/Main.java
@@ -0,0 +1,39 @@
+package com.tts;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Calculator calculator = new Calculator ();
+            MagicCalculator myMagic = new MagicCalculator();
+            Scanner sc = new Scanner (System.in);
+
+            System.out.print("Enter Number: ");
+
+            int num = sc.nextInt ();
+            int sq = calculator.square (num);
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            int add = calculator.add(num1, num2);
+
+            System.out.println("The sum of the numbers: "+ num1+ "+" + num2 +"=" + add);
+            System.out.println("The differance of the numbers: "+ num1+ "-" + num2 +"=" + calculator.subtract(num1,num2));
+            System.out.println("The product of the numbers: "+ num1+ "X" + num2 +"=" + calculator.multiply(num1,num2));
+            System.out.println("The dividend of the numbers: "+ num1+ "/" + num2 +"=" + calculator.divide(num1,num2));
+            System.out.println ("The Square Root of " + num + " is: " + sq);
+            System.out.println ("Enter angle in degrees :");
+
+            double degree = sc.nextDouble ();
+            double radians = Math.toRadians (degree);
+
+            System.out.println ("Sin is: " + Math.sin (radians));
+            System.out.println ("Cosine is: " + Math.cos (radians));
+            System.out.println ("Tangent is: " + Math.tan (radians));
+            System.out.println ("The Factorial of " + num + " is " + myMagic.factorial(num));
+    }
+
+
+}
+
